8ad723127c05e34d7acd98f40580b90faa7ac096
Jimmy Yaguache <jyaguache1989@gmail.com>
2023-06-30 00:09:41 +0000

    Doc API

--- main.py
+++ main.py
@@ -1,8 +1,67 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
+import uuid
 
-app = FastAPI()
+from fastapi_versioning import VersionedFastAPI, version
+
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+from auth import authenticate
+
+#seccion mongo importar libreria
+import pymongo
+
+import spotipy
+
+sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyClientCredentials(
+    client_id='c8519595485648c3949369793de3e366',
+    client_secret='d266e54ea24346a7b278445be87cd400'
+))
+
+description = """
+Utpl Interoperabilidad API - API Productos de un inventario. 🚀
+
+## Productos
+
+Tu puedes crear un producto.
+Tu puedes listar productos.
+
+
+## Tipos
+
+Usted podrá:
+
+* **Crear tipos de productos** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name":"productos",
+        "description": "Permite realizar un crud completo de un producto (listar)"
+    },
+    {
+        "name":"tipos",
+        "description": "Permite realizar un crud completo de tipos de productos"
+    },
+]
+
+app = FastAPI(
+    title="Utpl Interoperabilidad Tarea Sem-12",
+    description= description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Jimmy Yaguache",
+        "url": "http://x-force.example.com/contact/",
+        "email": "jjyaguache1@utpl.edu.ec",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags = tags_metadata
+)
 
 class Producto (BaseModel):
     id: int
